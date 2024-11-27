df97dec7a5de0a940db70d9c690140d92c783eb6
grazielle157 <00001086769831sp@al.educacao.sp.gov.br>
2024-11-27 11:05:37 -0300

    projeto

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a maior montanha do mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o lado do corpo que o coração fica localizado?',
+    'Esquerdo'
+)
+
+criaCartao(
+    'História',
+    'Quem formava a Triplice Entente na primeira guerra mundial?',
+    'França, Inglaterra e Russia'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz verde em Inglês?',
+    'Green'
+)
